8722fe0ca42cff18dd335cc5468a8c4c046dca7a
Michael Li <alimy@gility.net>
2019-01-12 18:56:14 +0800

    add example submodule

---
+++ module/example/go.mod
@@ -0,0 +1,7 @@
+module github.com/alimy/mir/module/example
+
+require (
+	github.com/alimy/mir v0.0.0-20190112095807-cae71e3b762c
+	github.com/alimy/mir/module/gin v0.0.0-20190112105350-8d19d02e99ce
+	github.com/gin-gonic/gin v1.3.0
+)
---
+++ module/example/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/alimy/mir"
+	"github.com/gin-gonic/gin"
+	"net/http"
+
+	ginE "github.com/alimy/mir/module/gin"
+)
+
+type site struct {
+	v1       mir.Group `mir:"v1"`
+	index    mir.Get   `mir:"/index/"`
+	articles mir.Get   `mir:"/articles/:category/#GetArticles"`
+}
+
+// Index handler of the index field that in site struct, the struct tag indicate
+// this handler will register to path "/index/" and method is http.MethodGet.
+func (h *site) Index(context *gin.Context) {
+	context.String(http.StatusOK, "get index data")
+}
+
+// GetArticles handler of articles indicator that contains Host/Path/Queries/Handler info.
+func (h *site) GetArticles(context *gin.Context) {
+	context.String(http.StatusOK, "get articles data")
+}
+
+func main() {
+	engine := gin.Default()
+
+	// Setup mir engine
+	mir.Setup(ginE.Mir(engine))
+
+	// Register handler to engine by mir
+	if err := mir.Register(&site{}); err != nil {
+		panic(err)
+	}
+
+	// Start gin engine serve
+	engine.Run()
+}
