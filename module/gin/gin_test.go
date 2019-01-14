f1ab38c0e9b0aafb67999cfdbebb4385b417c93b
Michael Li <alimy@gility.net>
2019-01-15 00:29:39 +0800

    gin: fixed error when method name<4

--- module/gin/gin_test.go
+++ module/gin/gin_test.go
@@ -14,20 +14,26 @@ import (
 type site struct {
 	count    uint64
 	v1       mir.Group `mir:"v1"`
+	add      mir.Post  `mir:"/add/:id"`
 	index    mir.Get   `mir:"/index/"`
 	articles mir.Get   `mir:"/articles/:category/#GetArticles"`
+}
+
+// Add handler of "/add/:id"
+func (h *site) Add(context *gin.Context) {
+	context.String(http.StatusOK, "add")
 }
 
 // Index handler of the index field that in site struct, the struct tag indicate
 // this handler will register to path "/index/" and method is http.MethodGet.
 func (h *site) Index(context *gin.Context) {
 	h.count++
-	context.String(http.StatusOK, "get index data")
+	context.String(http.StatusOK, "Index")
 }
 
 // GetArticles handler of articles indicator that contains Host/Path/Queries/Handler info.
 func (h *site) GetArticles(context *gin.Context) {
-	context.String(http.StatusOK, "get articles data")
+	context.String(http.StatusOK, "GetArticles")
 }
 
 func TestMir(t *testing.T) {
