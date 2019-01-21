8c5e821828ddc90baae46a6c10f407542065615c
Michael Li <alimy@gility.net>
2019-01-21 16:44:21 +0800

    example:update code and go.mod

--- module/example/go.mod
+++ module/example/go.mod
@@ -1,7 +1,7 @@
 module github.com/alimy/mir/module/example
 
 require (
-	github.com/alimy/mir v0.5.1-0.20190119172507-476d88190b52
-	github.com/alimy/mir/module/gin v0.0.0-20190119173040-ab7801c02a7b
+	github.com/alimy/mir v0.5.2-0.20190121081835-6a12746c47e2
+	github.com/alimy/mir/module/gin v0.0.0-20190121083822-23fc6ffddb60
 	github.com/gin-gonic/gin v1.3.0
 )
--- module/example/main.go
+++ module/example/main.go
@@ -21,13 +21,13 @@ type site struct {
 
 // Index handler of the index field that in site struct, the struct tag indicate
 // this handler will register to path "/index/" and method is http.MethodGet.
-func (h *site) Index(context *gin.Context) {
-	context.String(http.StatusOK, "get index data")
+func (s *site) Index(c *gin.Context) {
+	c.String(http.StatusOK, "Index")
 }
 
 // GetArticles handler of articles indicator that contains Host/Path/Queries/Handler info.
-func (h *site) GetArticles(context *gin.Context) {
-	context.String(http.StatusOK, "get articles data")
+func (s *site) GetArticles(c *gin.Context) {
+	c.String(http.StatusOK, "GetArticles:"+c.Param("category"))
 }
 
 func main() {
