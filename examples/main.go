02dc884023cc73f14d360ccdb640ad68845aa793
alimy <alimy@gility.net>
2020-02-21 13:29:00 +0800

    simply examples code

--- examples/main.go
+++ examples/main.go
@@ -7,22 +7,33 @@ package main
 import (
 	"log"
 
-	"github.com/alimy/mir/v2/examples/mirc/gen/api"
-	"github.com/alimy/mir/v2/examples/servants"
 	"github.com/gin-gonic/gin"
 
-	apiV1 "github.com/alimy/mir/v2/examples/mirc/gen/api/v1"
-	apiV2 "github.com/alimy/mir/v2/examples/mirc/gen/api/v2"
+	"github.com/alimy/mir/v2/examples/mirc/gen/api"
+	"github.com/alimy/mir/v2/examples/mirc/gen/api/v1"
+	"github.com/alimy/mir/v2/examples/mirc/gen/api/v2"
+	"github.com/alimy/mir/v2/examples/servants"
 )
 
 func main() {
 	e := gin.New()
-	// register servant to engine
-	api.RegisterSiteServant(e, servants.EmptySiteWithNoGroup{})
-	apiV1.RegisterSiteServant(e, servants.EmptySiteV1{})
-	apiV2.RegisterSiteServant(e, servants.EmptySiteV2{})
+
+	// register servants to engine
+	registerServants(e)
+
 	// start servant service
 	if err := e.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func registerServants(e *gin.Engine) {
+	// register default group routes
+	api.RegisterSiteServant(e, servants.EmptySiteWithNoGroup{})
+
+	// register routes for group v1
+	v1.RegisterSiteServant(e, servants.EmptySiteV1{})
+
+	// register routes for group v2
+	v2.RegisterSiteServant(e, servants.EmptySiteV2{})
 }
