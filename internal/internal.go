fd575d4ff25764961c7c6abc3e6c50d16d62e110
alimy <alimy@gility.net>
2020-03-07 15:21:53 +0800

    just add a dummy internal file in root

---
+++ internal/internal.go
@@ -0,0 +1,5 @@
+// Copyright 2020 Michael Li <alimy@gility.net>. All rights reserved.
+// Use of this source code is governed by Apache License 2.0 that
+// can be found in the LICENSE file.
+
+package internal
