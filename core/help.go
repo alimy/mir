3c58a110529bebc0b3985eb76805ac2768d2bb21
alimy <alimy@niubiu.com>
2025-06-19 13:26:24 +0800

    change license title's email info from alimy@gility.net to alimy@niubiu.com

--- core/help.go
+++ core/help.go
@@ -1,3 +1,7 @@
+// Copyright 2025 Michael Li <alimy@niubiu.com>. All rights reserved.
+// Use of this source code is governed by Apache License 2.0 that
+// can be found in the LICENSE file.
+
 package core
 
 func AssertTypeSpec(style string) (spec string, imports []string) {
--- examples/sail-example/mirc/web/site.go
+++ examples/sail-example/mirc/web/site.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Michael Li <alimy@gility.net>. All rights reserved.
+// Copyright 2025 Michael Li <alimy@niubiu.com>. All rights reserved.
 // Use of this source code is governed by Apache License 2.0 that
 // can be found in the LICENSE file.
 
